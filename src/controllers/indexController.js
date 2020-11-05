3c62e0a94eac09e5e9eae94d2a49756748cc45fb
gonzalozevallos <gonzalo.zev@gmail.com>
2020-11-05 19:03:19 -0300

    creamos la preferencia

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,8 +1,72 @@
+const { response } = require("express");
+const mercadopago = require("mercadopago");
+
+mercadopago.configure({
+    integrator_id: "dev_24c65fb163bf11ea96500242ac130004",
+    access_token: "APP_USR-6317427424180639-042414-47e969706991d3a442922b0702a0da44-469485398"
+});
+
 module.exports = {
     home: (req, res) => {
         return res.render("index");
     },
     detail: (req, res) => {
-        return res.render("detail", { ...req.query });
+
+        const preferenceObj = {
+            items: [
+                {
+                    id: "1234",
+                    title: req.query.title,
+                    picture_url: req.query.img,
+                    description: "Calzado con muucha facha",
+                    quantity: 1
+                }
+            ],
+            payer: {
+                name: "Ryan",
+                surname: "Dahl",
+                email: "test_user_63274575@testuser.com",
+                phone: {
+                    area_code: "11",
+                    number: "55556666"
+                },
+                address: {
+                    zip_code: "1234",
+                    street_name: "Monroe",
+                    street_number: 860
+                }
+            },
+            payment_methods: {
+                excluded_payment_methods: [
+                    {
+                        id: "visa"
+                    },
+                    {
+                        id: "debvisa"
+                    }
+                ],
+                excluded_payment_types:  [
+                    {
+                        id: "atm"
+                    }
+                ],
+                installments: 12
+            },
+            back_urls: {
+                success: "https://gonzalozevallos-taller-mp-dh.herokuapp.com/success",
+                pending: "https://gonzalozevallos-taller-mp-dh.herokuapp.com/pending",
+                failure: "https://gonzalozevallos-taller-mp-dh.herokuapp.com/failure"
+            },
+            auto_return: "approved",
+            external_reference: "gonzalo.zev@gmail.com"
+        }
+
+        mercadopago.preferences.create(preferenceObj)
+            .then(response => {
+                const preference = response.body;
+
+                return res.render("detail", { ...req.query, preference });
+            })
+
     }
 }
