6d8205e0ba716fcc70683bfa7be5e2b1e7dd9a35
gonzalozevallos <gonzalo.zev@gmail.com>
2020-11-11 18:48:58 -0300

    lkj

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(bodyParser())
+// app.use(bodyParser())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
