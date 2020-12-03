826f76aa78a8501962d59d20d715863126998154
Tomas <tomijais@gmail.com>
2020-12-03 14:54:51 -0300

    clase 3 para heroku

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -13,8 +13,36 @@ module.exports = {
   detail: (req, res) => {
     return res.render("detail", { ...req.query });
   },
+  callback: (req, res) => {
+    console.log(req.query);
+    if (req.query.status.includes("success")) {
+      return res.render("succes");
+    }
+    if (req.query.status.includes("pending")) {
+      return res.render("pending");
+    }
+    if (req.query.status.includes("failure")) {
+      return res.render("failure");
+    }
+    return res.status(404).end();
+  },
+  notifications: (req, res) => {
+    console.log(req.body);
+    res.status(200).end("Ok");
+  },
   buy: (req, res) => {
+    const host = "http://localhost:3000/";
+    const url = host + "callback?status=";
+    
+
     let preference = {
+      back_urls: {
+        success: url + "success",
+        pending: url + "pending",
+        failure: url + "failure",
+      },
+      notification_url: host + "notifications",
+      auto_return: "approved",
       payer: {
         name: "Ryan",
         surname: "Dahl",
@@ -24,10 +52,10 @@ module.exports = {
           number: 55556666,
         },
         address: {
-            zip_code:"1234",
-            street_name:"Monroe",
-            street_number:860,
-        }
+          zip_code: "1234",
+          street_name: "Monroe",
+          street_number: 860,
+        },
       },
       payment_methods: {
         excluded_payment_methods: [{ id: "visa" }],
@@ -36,11 +64,12 @@ module.exports = {
       },
       items: [
         {
-          id: "1",
+          id: 1234,
           picture_url:
             "https://taller-mercado-pago.herokuapp.com/images/products/jordan.jpg",
           title: "Nombre del Producto",
-          unit_price: 999,
+          description: "Dispositivo móvil de Tienda e-commerce",
+          unit_price: Number("999"),
           quantity: 3,
         },
       ],
--- src/routes/index.js
+++ src/routes/index.js
@@ -12,4 +12,11 @@ router.get('/detail', indexController.detail);
 // POST comprar
 
 router.post("/buy", indexController.buy)
+
+// POST router
+
+router.get('/callback', indexController.callback)
+
+router.post('/notifications', indexController.notifications)
+
 module.exports = router;
